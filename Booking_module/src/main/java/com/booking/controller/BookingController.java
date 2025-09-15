f5afd8040dffd37db814ef95df771cb57ef33e4b
Sajid18May <sajid1852002@gmail.com>
2025-09-15 22:36:16 +0530

    added controller module

--- Booking_module/src/main/java/com/booking/controller/BookingController.java
+++ Booking_module/src/main/java/com/booking/controller/BookingController.java
@@ -1,5 +1,6 @@
 package com.booking.controller;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,6 +32,47 @@ public class BookingController {
 		APIResponse<Room> room = bookingClient.getRoom(bookingDto.getRoomId());
 		APIResponse<List<RoomAvailability>> roomAvailibility = bookingClient
 				.getRoomAvailibility(bookingDto.getRoomAvailabilityId());
+		
+		List<RoomAvailability> availableRooms=roomAvailibility.getData();
+		
+		//Logic to check available rooms based on date and count
+				for(LocalDate date: bookingDto.getDate()) {
+					boolean isAvailable = availableRooms.stream()
+					        .anyMatch(ra -> ra.getAvailableDate().equals(date) && ra.getAvailableCount()>0);
+					
+					    
+					    System.out.println("Date " + date + " available: " + isAvailable);
+					    
+					    if (!isAvailable) {
+					    	 messages.add("Room not available on: " + date);
+					    	 apiResponse.setMessage("Sold Out");
+					 		 apiResponse.setStatus(500);
+					 		 apiResponse.setData(messages);
+					 		 return apiResponse;
+					    }
+					    
+				}
+				//Save it to Booking Table with status pending
+//				Bookings bookings = new Bookings();
+//				bookings.setName(bookingDto.getName());
+//				bookings.setEmail(bookingDto.getEmail());
+//				bookings.setMobile(bookingDto.getMobile());
+//				bookings.setPropertyName(response.getData().getName());
+//				bookings.setStatus("pending");
+//				bookings.setTotalPrice(roomType.getData().getBasePrice()*bookingDto.getTotalNigths());
+//				Bookings savedBooking = bookingRepository.save(bookings);
+//				
+//				for(LocalDate date: bookingDto.getDate()) {
+//					BookingDate  bookingDate = new BookingDate();
+//					bookingDate.setDate(date);
+//					bookingDate.setBookings(savedBooking);
+//					bookingDateRepository.save(bookingDate);
+//				}
+//				
+				
+				
+				
+				return null;
 	}
 
 }
